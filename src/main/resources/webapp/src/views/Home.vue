2d0591a98229d946f674e54afb750bdb61222e67
Michael Parker <me.parker1992@gmail.com>
2021-03-23 01:04:01 +1100

    add dummy frontend

---
+++ src/main/resources/webapp/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="hello">
+        <h1>{{ title }}</h1>
+        <h2>Balance: {{ balance }}</h2>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    props: {
+      title: String
+    },
+    data: function () {
+      return {
+        balance: 0
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
