4e87a8a96de981d81065bee834388e4d1226d37a
Michael Parker <me.parker1992@gmail.com>
2021-03-23 02:06:40 +1100

    add simple frontent with balance

--- src/main/resources/webapp/src/main.js
+++ src/main/resources/webapp/src/main.js
@@ -1,10 +1,12 @@
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 import Vue from 'vue'
 import App from './App.vue'
 import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
+import Axios from 'axios'
 
 Vue.use(VueMaterial)
+Vue.prototype.$http = Axios
 
 Vue.config.productionTip = false
 
--- src/main/resources/webapp/src/views/Home.vue
+++ src/main/resources/webapp/src/views/Home.vue
@@ -15,6 +15,23 @@
       return {
         balance: 0
       }
+    },
+    mounted () {
+      this.getBalance()
+    },
+    methods: {
+      getBalance() {
+        const self = this
+        const url = `http://localhost:8081/api/account/balance`
+        this.$http.get(url).then(function (response) {
+          console.log(response.data)
+          self.balance = response.data.amount
+        }).catch(function (error) {
+          self.balance = 50
+          console.log(error)
+        })
+      }
+
     }
   }
 </script>
